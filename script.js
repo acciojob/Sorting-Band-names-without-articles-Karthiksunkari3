54707a945edda1152ca8b78e5ff9b2e6468c8400
Karthik Sunkari <84893245+Karthiksunkari3@users.noreply.github.com>
2023-06-24 23:49:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,36 @@
 //your code here
+let bandNames = ['The Beatles', 'Led Zeppelin', 'The Rolling Stones', 'Nirvana', 'Pink Floyd', 'Queen'];
+
+// Function to compare band names while sorting
+function compareBandNames(a, b) {
+  // Remove articles from band names for comparison
+  const articleRegex = /^(a|an|the)\s+/i;
+  const bandNameA = a.replace(articleRegex, '');
+  const bandNameB = b.replace(articleRegex, '');
+
+  // Compare the modified band names
+  if (bandNameA < bandNameB) {
+    return -1;
+  }
+  if (bandNameA > bandNameB) {
+    return 1;
+  }
+  return 0;
+}
+
+// Sort the band names in lexicographic order excluding articles
+bandNames.sort(compareBandNames);
+
+// Get the 'band' ul element
+const bandList = document.getElementById('band');
+
+// Clear any existing content inside the 'band' ul
+bandList.innerHTML = '';
+
+// Add each band name as an 'li' element to the 'band' ul
+bandNames.forEach((name) => {
+  const listItem = document.createElement('li');
+  listItem.textContent = name;
+  bandList.appendChild(listItem);
+});
 
